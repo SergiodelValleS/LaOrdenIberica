aedd54941e4191a8cebee258714654080cb7bcdb
SergiodelValleS <sergiodvsva@gmail.com>
2026-02-16 21:01:36 +0100

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,15 +2,14 @@
 <nav>
         <div class='container'>
             <div class='row'>
-            <a href='https://laordeniberica.free.nf/' class='a-nav col'>
-    			<div class='col' style='border:1px solid white;' title='Inicio'><h1>Inicio</h1></div>
-    	    </a>
-            <a href='https://laordeniberica.free.nf/contacto' class='a-nav col'>
-    		<div class='col' style='border:1px solid white;' title='página de contacto'><h1>Contacto</h1></div>
-    	    </a>
-            <a href='https://docs.google.com/forms/d/e/1FAIpQLSd_VipdP6dYbkp6csdMfOBhFMvWCmX-gq25TGkV9iRkZV94kg/viewform?usp=sharing&ouid=114792237868050755748' class='a-nav col'>
-    			<div class='col' style='border:1px solid white;' title='página de sugerencias'><h1>Sugerencias</h1></div>
-    	    </a>
+            <div class='col divNav' style='border:1px solid white;'title='Inicio'>
+    			<a href='https://laordeniberica.free.nf/' class='a-nav'><h1>Inicio</h1></a></div>
+            <div class='col divNav' style='border:1px solid white;' title='página de contacto'>
+    			<a href='https://laordeniberica.free.nf/contacto' class='a-nav'><h1>Contacto</h1>
+    			</a></div>
+    <div class='col divNav' style='border:1px solid white;' title='página de sugerencias'>
+    <a href='https://docs.google.com/forms/d/e/1FAIpQLSd_VipdP6dYbkp6csdMfOBhFMvWCmX-gq25TGkV9iRkZV94kg/viewform?usp=sharing&ouid=114792237868050755748' class='a-nav'><h1>Sugerencias</h1></a></div>
+            </div>
         </div>
     </nav>
 </header>
