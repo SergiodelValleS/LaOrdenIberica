7f220e399120becea4b3056674ef8c1733b0f0d0
SergiodelValleS <sergiodvsva@gmail.com>
2026-02-16 20:38:36 +0100

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,11 +2,15 @@
 <nav>
         <div class='container'>
             <div class='row'>
-            <div class='col' style='border:1px solid white;' onclick="window.location.href='https://laordeniberica.free.nf/';" title='Inicio'><a href='https://laordeniberica.free.nf/' class='a-nav'><h1>Inicio</h1></a></div>
-            <!--<div class='col' style='border:1px solid white;' onclick="window.location.href='https://laordeniberica.free.nf/sugerencias';" title='pagina de sugerencia'><a href='https://laordeniberica.free.nf/sugerencias' class='a-nav'><h1>Sugerencias</h1></a></div>-->
-            <div class='col' style='border:1px solid white;' onclick="window.location.href='https://laordeniberica.free.nf/contacto';" title='página de contacto'><a href='https://laordeniberica.free.nf/contacto' class='a-nav'><h1>Contacto</h1></a></div>
-    <div class='col' style='border:1px solid white;' onclick="window.location.href='https://laordeniberica.free.nf/contacto';" title='página de sugerencias'><a href='https://docs.google.com/forms/d/e/1FAIpQLSd_VipdP6dYbkp6csdMfOBhFMvWCmX-gq25TGkV9iRkZV94kg/viewform?usp=sharing&ouid=114792237868050755748' class='a-nav'><h1>Sugerencias</h1></a></div>
-            </div>
+            <a href='https://laordeniberica.free.nf/' class='a-nav col'>
+    			<div class='col' style='border:1px solid white;' title='Inicio'><h1>Inicio</h1></div>
+    	    </a>
+            <a href='https://laordeniberica.free.nf/contacto' class='a-nav col'>
+    		<div class='col' style='border:1px solid white;' title='página de contacto'><h1>Contacto</h1></div>
+    	    </a>
+            <a href='https://docs.google.com/forms/d/e/1FAIpQLSd_VipdP6dYbkp6csdMfOBhFMvWCmX-gq25TGkV9iRkZV94kg/viewform?usp=sharing&ouid=114792237868050755748' class='a-nav col'>
+    			<div class='col' style='border:1px solid white;' title='página de sugerencias'><h1>Sugerencias</h1></div>
+    	    </a>
         </div>
     </nav>
 </header>
